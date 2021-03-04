a79726c0dc211f4035db0db87dd4cac1a062c2e4
Sebastien Cheraux <scheraux@gmail.com>
2021-03-03 18:09:05 -0800

    render comments by status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === 'pending') {
+      content = 'comment is awaiting moderation';
+    }
+
+    if (comment.status === 'rejected') {
+      content = 'comment has been rejected';
+    }
+
+    if (comment.status === 'approved') {
+      content = comment.content;
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
